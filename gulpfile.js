112e06a6638b9c92fc133ed220d1026b81c1f740
levithomason <me@levithomason.com>
2015-03-07 15:52:03 -0800

    gulp server, renaming

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,43 @@
     var gulp = require('gulp');
     var autoprefixer = require('gulp-autoprefixer');
     var less = require('gulp-less');
-
+    var webserver = require('gulp-webserver');
+    
+    var paths = {
+        root: './'
+    };
+    
     gulp.task('less', function () {
         return gulp.src([
-            './start-to-finish.less'
+            paths.root + '*.less'
         ])
             .pipe(less())
             .pipe(autoprefixer())
             .pipe(gulp.dest('.'));
     });
 
-    gulp.task('watch', function(cb) {
-        gulp.watch([
-            './start-to-finish.less'
-        ], ['less']);
+    gulp.task('watch', function() {
+        gulp.watch(paths.root + '*.less', ['less']);
     });
+    
+    gulp.task('serve', function() {
+        return gulp.src(paths.root)
+            .pipe(webserver({
+                https: false,
+                host: 'localhost',
+                port: '8000',
+                fallback: 'index.html',
+                livereload: true,
+                directoryListing: false,
+                open: true,
+            }));
+        
+    });
+    
+    gulp.task('default', [
+        'less',
+        'serve',
+        'watch',
+    ]);
 
 }());
