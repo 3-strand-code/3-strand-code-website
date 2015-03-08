fc0a104b6f3cb8c3bab2d8cb1c9e609531dc13d2
levithomason <me@levithomason.com>
2015-03-07 16:51:18 -0800

    adds plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
-(function () {
+(function() {
     'use strict';
 
     var gulp = require('gulp');
     var autoprefixer = require('gulp-autoprefixer');
     var less = require('gulp-less');
     var webserver = require('gulp-webserver');
-    
+    var plumber = require('gulp-plumber');
+
     var paths = {
         root: './'
     };
-    
-    gulp.task('less', function () {
+
+
+    gulp.task('less', function() {
         return gulp.src([
             paths.root + '*.less'
         ])
+            .pipe(plumber(function onError(err) {
+                    console.log(err.message);
+                    this.emit('end');
+                }
+            ))
             .pipe(less())
             .pipe(autoprefixer())
             .pipe(gulp.dest('.'));
@@ -22,7 +29,7 @@
     gulp.task('watch', function() {
         gulp.watch(paths.root + '*.less', ['less']);
     });
-    
+
     gulp.task('serve', function() {
         return gulp.src(paths.root)
             .pipe(webserver({
@@ -32,11 +39,11 @@
                 fallback: 'index.html',
                 livereload: true,
                 directoryListing: false,
-                open: true,
+                open: false,
             }));
-        
+
     });
-    
+
     gulp.task('default', [
         'less',
         'serve',
