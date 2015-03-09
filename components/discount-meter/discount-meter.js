2320d1c8a926df4923eb8b31784ec0bc50d9e4b5
Levi Thomason <me@levithomason.com>
2015-03-08 18:48:29 -0700

    register form, replaces lorem

--- components/discount-meter/discount-meter.js
+++ components/discount-meter/discount-meter.js
@@ -4,15 +4,15 @@
     angular.module('App')
 
         .directive('discountMeter', [
-            '$interval',
-            function($interval) {
+            '$interval', '$filter',
+            function($interval, $filter) {
                 return {
                     restrict: 'E',
                     replace: true,
                     templateUrl: 'components/discount-meter/discount-meter.html',
                     scope: {},
                     link: function(scope, elem, attrs) {
-                        scope._fullDiscount = 0.5;
+                        scope._fullDiscount = 50;
 
                         scope._startDate = new Date(2015, 2, 7);
                         scope._endDate = new Date(2015, 2, 31);
@@ -27,16 +27,19 @@
                                 scope._remainingRate = 1;
                             }
 
+                            scope._remainingDiscount = scope._fullDiscount * scope._remainingRate;
 
-                            scope.remainingDiscount = scope._fullDiscount * scope._remainingRate;
+                            // separate whole numbers and decimal values
+                            scope.discountWholeNumbers = Math.floor($filter('number')(scope._remainingDiscount, 2));
+                            scope.discountDecimalNumbers = String(scope._remainingDiscount).split('.')[1].substr(0, 6);
 
-                            scope._markerBottom = scope._remainingRate * 100 + '%';
+                            scope._markerTop = 100 * (1 - scope._remainingRate) + '%';
 
                         }, 30);
 
                         scope.discount = new Date();
                     }
-                }
+                };
             }]);
 
 }());
