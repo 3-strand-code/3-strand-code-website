53a42b1e35ec5eed305a0887d0cb03de2ecab9b6
Levi Thomason <me@levithomason.com>
2015-03-11 08:01:35 -0700

    gets discounts

--- components/discount-meter/discount-meter.js
+++ components/discount-meter/discount-meter.js
@@ -13,7 +13,7 @@
                     scope: {},
                     link: function(scope, elem, attrs) {
                         
-                        $kinvey.execute('remaining-discounts')
+                        $kinvey.execute('discounts')
                             .then(function(discounts) {
                                 scope.discounts = discounts;
                                 console.log(scope.discounts);
@@ -22,33 +22,35 @@
                             });
 
 
-                        scope._fullDiscount = 50;
+                        // Time based discount, deprecated
                         
-                        scope._startDate = new Date(2015, 2, 7);
-                        scope._endDate = new Date(2015, 2, 31);
-                        scope._discountPeriod = scope._endDate - scope._startDate;
-                        
-                        $interval(function() {
-                            scope._now = new Date();
-                            scope._remainingPeriod = scope._endDate - scope._now;
-                            scope._remainingRate = scope._remainingPeriod / scope._discountPeriod;
-                        
-                            if (scope._remainingRate > 1) {
-                                scope._remainingRate = 1;
-                            }
-                        
-                            scope._remainingDiscount = scope._fullDiscount * scope._remainingRate;
-                        
-                            // separate whole numbers and decimal values
-                            // todo: whole number is busted, 46.000 never rolled to 45.999 but stayed at 46.999
-                            scope.discountWholeNumbers = Math.floor($filter('number')(scope._remainingDiscount, 2));
-                            scope.discountDecimalNumbers = String(scope._remainingDiscount).split('.')[1].substr(0, 6);
-                        
-                            scope._markerTop = 100 * (1 - scope._remainingRate) + '%';
-                        
-                        }, 30);
-                        
-                        scope.discount = new Date();
+                        //scope._fullDiscount = 50;
+                        //
+                        //scope._startDate = new Date(2015, 2, 7);
+                        //scope._endDate = new Date(2015, 2, 31);
+                        //scope._discountPeriod = scope._endDate - scope._startDate;
+                        //
+                        //$interval(function() {
+                        //    scope._now = new Date();
+                        //    scope._remainingPeriod = scope._endDate - scope._now;
+                        //    scope._remainingRate = scope._remainingPeriod / scope._discountPeriod;
+                        //
+                        //    if (scope._remainingRate > 1) {
+                        //        scope._remainingRate = 1;
+                        //    }
+                        //
+                        //    scope._remainingDiscount = scope._fullDiscount * scope._remainingRate;
+                        //
+                        //    // separate whole numbers and decimal values
+                        //    // todo: whole number is busted, 46.000 never rolled to 45.999 but stayed at 46.999
+                        //    scope.discountWholeNumbers = Math.floor($filter('number')(scope._remainingDiscount, 2));
+                        //    scope.discountDecimalNumbers = String(scope._remainingDiscount).split('.')[1].substr(0, 6);
+                        //
+                        //    scope._markerTop = 100 * (1 - scope._remainingRate) + '%';
+                        //
+                        //}, 30);
+                        //
+                        //scope.discount = new Date();
                     }
                 };
             }]);
--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 var livereload = require('express-livereload');
 var logfmt = require('logfmt');
 var pkg = require('./package.json');
+var request = require('request');
 
 var SERVER_ROOT = __dirname + '/build/';
+
+var kinveyAppKey = process.env.TSC_KINVEY_APP_KEY;
+var kinveyAppSecret = process.env.TSC_KINVEY_APP_SECRET;
+var kinveyMasterSecret = process.env.TSC_KINVEY_MASTER_SECRET;
 
 //////////////////////////////////////////////////////////////////
 // SETUP APP
@@ -39,4 +44,23 @@ app.use('/', express.static(__dirname + '/'));
 // let angular handle all routing requests (html5mode)
 app.all('/*', function (req, res) {
     res.sendFile(__dirname + '/build/index.html');
+});
+
+
+var url = 'https://baas.kinvey.com/rpc/' + kinveyAppKey + '/custom/discounts';
+
+var headers = {
+    'Authorization': 'Basic ' + new Buffer(kinveyAppKey + ':' + kinveyMasterSecret).toString('base64'),
+    'Content-Type': 'application/json'
+};
+
+var form = {};
+
+request.post({url: url, form: form, headers: headers}, function(error, response, body) {
+    //if (!error && response.statusCode == 200) {
+    //    console.log(body) // Show the HTML for the Google homepage.
+    //}
+    console.log(error);
+    //console.log(response);
+    console.log(body);
 });
