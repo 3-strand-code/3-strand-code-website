645e72514fd262b756f0d8972fcbb7039a4c8b62
levithomason <me@levithomason.com>
2015-03-15 14:35:03 -0700

    adds ssl, starts nav

--- web.js
+++ web.js
@@ -1,53 +1,92 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
 var livereload = require('express-livereload');
 var logfmt = require('logfmt');
 var pkg = require('./package.json');
 var request = require('request');
 
-var SERVER_ROOT = __dirname;
+var PROJECT_ROOT = __dirname;
+var SERVER_ROOT = PROJECT_ROOT;
 
-var kinveyAppKey = process.env.TSC_KINVEY_APP_KEY;
-var kinveyAppSecret = process.env.TSC_KINVEY_APP_SECRET;
-var kinveyMasterSecret = process.env.TSC_KINVEY_MASTER_SECRET;
+var KINVEY_APP_KEY = process.env.TSC_KINVEY_APP_KEY;
+var KINVEY_APP_SECRET = process.env.TSC_KINVEY_APP_SECRET;
+var KINVEY_MASTER_SECRET = process.env.TSC_KINVEY_MASTER_SECRET;
 
-//////////////////////////////////////////////////////////////////
-// SETUP APP
-//
+////
 
 var app = express();
 
+var httpPort = process.env.PORT || 8000;
+var httpsPort = process.env.PORT ? 443 : 8001;
+
+var sslOptions = {
+    key: fs.readFileSync('fixtures/dev-key.pem'),
+    cert: fs.readFileSync('fixtures/dev-cert.pem')
+};
+
+function onServerStart(port, https) {
+    console.log(https ? 'https' : 'http', 'listening on', port);
+}
+
+function ensureSecure(req, res, next) {
+    console.log('https://' + req.headers.host + ':' + httpsPort + req.url);
+    if (req.secure) {
+        return next();
+    }
+    res.redirect('https://' + req.headers.host + ':' + httpsPort + req.url);
+}
+
+/////////////////////////////////////////////////////////////
+// SETUP APP
+//
+
+//
+// Servers
+//
+http.createServer(app).listen(httpPort, onServerStart(httpPort));
+https.createServer(sslOptions, app).listen(httpsPort, onServerStart(httpsPort, true));
+
 //
 // Livereload
+
 livereload(app, {
-    // options: https://github.com/napcs/node-livereload#options
-    watchDir: SERVER_ROOT
+    watchDir: SERVER_ROOT,
+    https: sslOptions,
 });
 
-//////////////////////////////////////////////////////////////////
-// SETUP SERVER
 
+/////////////////////////////////////////////////////////////
+// Routing
 //
 
-var port = Number(process.env.PORT || 8000);
-var server = app.listen(port, function() {
-    console.log('Listening on ' + port);
-});
-
-// handle req body
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-
 //
-// logging
+// Logger
+// 
 app.use(logfmt.requestLogger());
 
+//
+// Parse Req Body
+// 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-// static serve
+//
+// Force HTTPS
+//
+app.all('*', ensureSecure);
+
+//
+// Serve Static
+// 
 app.use('/', express.static(__dirname + '/'));
 
 //
-// Payment
+// Payments
 //
 app.use('/charge', function(req, res) {
     // Set your secret key: remember to change this to your live secret key in production
@@ -68,14 +107,13 @@ app.use('/charge', function(req, res) {
     });
 });
 
-
 //
 // Custom kinvey endpoints
 // 
 app.use('/kinvey/:endpoint/', function(req, res) {
-    var url = 'https://baas.kinvey.com/rpc/' + encodeURIComponent(kinveyAppKey) + '/custom/' + req.params.endpoint;
+    var url = 'https://baas.kinvey.com/rpc/' + encodeURIComponent(KINVEY_APP_KEY) + '/custom/' + req.params.endpoint;
     var headers = {
-        'Authorization': 'Basic ' + new Buffer(kinveyAppKey + ':' + kinveyMasterSecret).toString('base64'),
+        'Authorization': 'Basic ' + new Buffer(KINVEY_APP_KEY + ':' + KINVEY_MASTER_SECRET).toString('base64'),
         'Content-Type': 'application/json'
     };
 
@@ -86,20 +124,18 @@ app.use('/kinvey/:endpoint/', function(req, res) {
     });
 });
 
-// let angular handle all routing requests (html5mode)
+//
+// Angular html5mode
+// 
 app.all('/*', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-//
+
+/////////////////////////////////////////////////////////////
 // Keep Alive
 // 
+
 setInterval(function() {
-    request.get("http://three-strand-code.herokuapp.com/keep-alive");
+    request.get("http://three-strand-code.herokuapp.com/");
 }, 300000);
-
-
-//
-// Stripe Token
-//
-
