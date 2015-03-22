a2aa144bb85f4c2ec5dd5e3afc859dfab9a9f9c9
Levi Thomason <me@levithomason.com>
2015-03-22 12:55:02 -0700

    conditional lr and https redirect

--- web.js
+++ web.js
@@ -8,6 +8,8 @@ var logfmt = require('logfmt');
 var pkg = require('./package.json');
 var request = require('request');
 
+var inProduction = process.env.NODE_ENV === 'production';
+
 var PROJECT_ROOT = __dirname;
 var SERVER_ROOT = PROJECT_ROOT;
 
@@ -15,9 +17,9 @@ var KINVEY_APP_KEY = process.env.TSC_KINVEY_APP_KEY;
 var KINVEY_APP_SECRET = process.env.TSC_KINVEY_APP_SECRET;
 var KINVEY_MASTER_SECRET = process.env.TSC_KINVEY_MASTER_SECRET;
 
-if (!KINVEY_APP_KEY) { throw 'KINVEY_APP_KEY not set.' }
-if (!KINVEY_APP_SECRET) { throw 'KINVEY_APP_SECRET not set.' }
-if (!KINVEY_MASTER_SECRET) { throw 'KINVEY_MASTER_SECRET not set.' }
+if (!KINVEY_APP_KEY)        throw 'KINVEY_APP_KEY not set.';
+if (!KINVEY_APP_SECRET)     throw 'KINVEY_APP_SECRET not set.';
+if (!KINVEY_MASTER_SECRET)  throw 'KINVEY_MASTER_SECRET not set.';
 
 ////
 
@@ -31,30 +33,7 @@ var sslOptions = {
     cert: fs.readFileSync('fixtures/dev-cert.pem')
 };
 
-function onServerStart(port, https) {
-    console.log(https ? 'https' : 'http', 'listening on', port);
-}
-
-function ensureSecure(req, res, next) {
-    if (req.secure) {
-        return next();
-    }
-    res.redirect('https://' + req.get('Host').split(':')[0] + ':' + httpsPort + req.url);
-}
-
-/////////////////////////////////////////////////////////////
-// SETUP APP
-//
-
-//
-// Servers
-//
-http.createServer(app).listen(httpPort, onServerStart(httpPort));
-https.createServer(sslOptions, app).listen(httpsPort, onServerStart(httpsPort, true));
-
-//
 // Livereload
-
 var lrOptions = {
     exts: [
         'html',
@@ -75,47 +54,78 @@ var lrOptions = {
     applyCSSLive: true,
     https: sslOptions,
 };
-var lrServer = livereload.createServer(lrOptions);
-lrServer.watch(SERVER_ROOT);
+
+function onServerStart(port, https) {
+    console.log(https ? 'https' : 'http', 'listening on', port);
+}
+
+function ensureSecure(req, res, next) {
+    if (req.secure) {
+        return next();
+    }
+
+    var redirectTo;
+    var host = req.get('Host').split(':')[0];
+    var port = req.get('Host').split(':')[1];
+
+    redirectTo = 'https://' + host + (port ? ':' + httpsPort : '') + req.url;
+
+    console.log(redirectTo);
+
+    res.redirect(redirectTo);
+}
+
+
+/////////////////////////////////////////////////////////////
+// SETUP APP
+
+// HTTP
+http.createServer(app).listen(httpPort, onServerStart(httpPort));
+
+// Local Dev
+if (!inProduction) {
+    // https
+    https.createServer(sslOptions, app).listen(httpsPort, onServerStart(httpsPort, true));
+
+    // livereload
+    livereload.createServer(lrOptions).watch(SERVER_ROOT);
+}
 
 
 /////////////////////////////////////////////////////////////
 // Routing
-//
+
 
 //
 // Logger
-// 
 app.use(logfmt.requestLogger());
+
 
 //
 // Parse Req Body
-// 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+
 //
 // Force HTTPS
-//
 app.all('*', ensureSecure);
+
 
 //
 // Serve Static
-// 
 app.use('/', express.static(__dirname + '/'));
+
 
 //
 // Payments
-//
 app.use('/charge', function(req, res) {
     // Set your secret key: remember to change this to your live secret key in production
     // See your keys here https://dashboard.stripe.com/account
     var stripe = require("stripe")("sk_test_0poqhPe77Ozg5bizoeEsMtX8");
-    console.log(req.body);
     var stripeToken = req.body.stripeToken;
-
     var charge = stripe.charges.create({
         amount: 24900,
         currency: "usd",
@@ -128,16 +138,15 @@ app.use('/charge', function(req, res) {
     });
 });
 
+
 //
 // Custom kinvey endpoints
-// 
 app.use('/kinvey/:endpoint/', function(req, res) {
     var url = 'https://baas.kinvey.com/rpc/' + encodeURIComponent(KINVEY_APP_KEY) + '/custom/' + req.params.endpoint;
     var headers = {
         'Authorization': 'Basic ' + new Buffer(KINVEY_APP_KEY + ':' + KINVEY_MASTER_SECRET).toString('base64'),
         'Content-Type': 'application/json'
     };
-
     var form = {};
 
     request.post({url: url, headers: headers, form: form}, function(error, response, body) {
@@ -145,9 +154,9 @@ app.use('/kinvey/:endpoint/', function(req, res) {
     });
 });
 
+
 //
 // Angular html5mode
-// 
 app.all('/*', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -155,7 +164,6 @@ app.all('/*', function(req, res) {
 
 /////////////////////////////////////////////////////////////
 // Keep Alive
-// 
 
 setInterval(function() {
     request.get("http://three-strand-code.herokuapp.com/");
